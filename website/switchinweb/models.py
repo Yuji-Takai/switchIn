995355b4c919ed7aa93a60715cd430d161f4c0ca
yujitakai <yuji21eagles0912@yahoo.co.jp>
2018-07-02 11:11:30 -0700

    organizing css files for the template

--- website/switchinweb/models.py
+++ website/switchinweb/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 
+# Coverage:             represents the coverage of user's policy
+# liability_property:   property damage liability limit
+# liability_injury:     bodily injury liability limit
+# personal_injury:      personal injury protectino deductible
+# comprehensive:        comprehensive deductible
+# collision:            collision deductible
+# uninsured_injury:     uninsured/underinsured motorist limit
 class Coverage(models.Model):
-    # property damage liability limit
     liability_property = models.CharField(max_length=200)
-    # bodily injury liability limit
     liability_injury = models.CharField(max_length=200)
-    # personal injury protection deductible
     personal_injury = models.CharField(max_length=200)
-    # comprehensive deductible
     comprehensive = models.CharField(max_length=200)
-    # collision deductible
     collision = models.CharField(max_length=200)
-    # uninsured/underinsured motorist injury limit
     uninsured_injury = models.CharField(max_length=200)
     # uninsured/underinsured motorist property damage limit
     #uninsured_property = models.CharField(max_length=200)
@@ -20,12 +21,25 @@ class Coverage(models.Model):
     # rental reimbursement
     #rental = models.CharField(max_length=200)
 
+# Vehicle:              represents the vehice covered under user's policy
+# year:                 year of the vehicle
+# make:                 make of the vehicle
+# model:                model of the vehicle
+# vin:                  VIN of the vehicle
 class Vehicle(models.Model):
     year = models.IntegerField()
     make = models.CharField(max_length=200)
     model = models.CharField(max_length=200)
     vin = models.CharField(max_length=30)
 
+# Policy:               represents the user's policy
+# company_name:         name of the insurance company
+# policy_number:        policy number of the policy
+# effective_date:       starting date of the policy
+# expiration_date:      end date of the policy
+# state:                state where the policy was issued
+# coverage:             coverage of the policy
+# vehicle:              vehicle covered by the policy
 class Policy(models.Model):
     company_name = models.CharField(max_length=200)
     policy_number = models.CharField(max_length=30)
@@ -34,7 +48,6 @@ class Policy(models.Model):
     state = models.CharField(max_length=20, null=True)
     coverage = models.OneToOneField(Coverage, on_delete=models.CASCADE, null=True)
     vehicle = models.OneToOneField(Vehicle, on_delete=models.CASCADE, null=True)
-    #document = models.FileField(upload_to='uploads/')
 
 class PolicyDoc(models.Model):
     document = models.FileField(upload_to='uploads/')
